e7d9ff1a489d9ea19f0fadd458de0d991885e0ef
rogerdpack <rogerpack2005@gmail.com>
2009-12-26 14:26:09 -0700

    passing specs

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,10 +1,25 @@
+require 'sane'
+require_rel '../lib/faster_gem_scripts'
+require 'spec/autorun'
+
 describe "rewriter" do
 
-  it "should overwrite a file" do
-
-
+  def setup
+    File.delete 'abc' if File.exist?('abc')
+    FasterGemScripts.overwrite 'abc'
   end
 
-  it "should nuke the cache on write"
+  it "should overwrite a file" do
+    setup
+    new_contents = File.read('abc')
+    assert new_contents.contain?("faster_rubygems")
+  end
+
+  it "should nuke the cache on write" do
+    require 'fileutils'
+    FileUtils.touch 'abc_bin_location'
+    setup
+    assert !File.exist?('abc_bin_location')
+  end
 
 end
